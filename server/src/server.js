197be2fac47a533ad45306476381d31687c779eb
Darby Ross <52804447+rossd84@users.noreply.github.com>
2023-10-02 16:14:06 -0500

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/testdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define User schema and model
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model('User', UserSchema);
+
+// Routes
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  
+  // Check if the username already exists
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ error: 'Username already exists' });
+  }
+  
+  // Hash the password before saving it
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  // Create a new user
+  const newUser = new User({
+    username,
+    password: hashedPassword,
+  });
+  
+  await newUser.save();
+  res.status(201).json({ message: 'User registered successfully' });
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  
+  // Find the user by username
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+  
+  // Compare the hashed password
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+  
+  // Store user information in the session
+  req.session.user = user;
+  
+  res.status(200).json({ message: 'Login successful' });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.status(200).json({ message: 'Logged out successfully' });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
