ff5d71884c6388306d26fcd40911e8da2d34e810
Darby Ross <52804447+rossd84@users.noreply.github.com>
2023-10-02 17:19:24 -0500

    add passport
    add passport and change routes to use passport.

--- server/src/server.js
+++ server/src/server.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
-
 require('dotenv').config();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({ secret: process.env.SECRET_SESSION_KEY, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -30,53 +33,82 @@ const UserSchema = new mongoose.Schema({
 });
 const User = mongoose.model('User', UserSchema);
 
+// Passport Local Strategy Configuration
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return done(null, false, { message: 'Invalid username or password' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return done(null, false, { message: 'Invalid username or password' });
+    }
+
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}));
+
+// Passport Serialization/Deserialization
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 // Routes
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  
+
   // Check if the username already exists
   const existingUser = await User.findOne({ username });
   if (existingUser) {
     return res.status(400).json({ error: 'Username already exists' });
   }
-  
+
   // Hash the password before saving it
   const hashedPassword = await bcrypt.hash(password, 10);
-  
+
   // Create a new user
   const newUser = new User({
     username,
     password: hashedPassword,
   });
-  
+
   await newUser.save();
   res.status(201).json({ message: 'User registered successfully' });
 });
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  
-  // Find the user by username
-  const user = await User.findOne({ username });
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid username or password' });
-  }
-  
-  // Compare the hashed password
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) {
-    return res.status(401).json({ error: 'Invalid username or password' });
-  }
-  
-  // Store user information in the session
-  req.session.user = user;
-  
-  res.status(200).json({ message: 'Login successful' });
-});
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/dashboard', // Redirect to dashboard on successful login
+  failureRedirect: '/login',     // Redirect to login page on failed login
+}));
 
 app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.status(200).json({ message: 'Logged out successfully' });
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    // res.redirect('/login');
+    res.status(201).json({ message: 'User logged out successfully' });
+  });
+});
+
+// Example protected route
+app.get('/dashboard', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ error: 'Authentication required' });
+  }
+  res.status(200).json({ message: 'Welcome to your dashboard' });
 });
 
 app.listen(3000, () => {
